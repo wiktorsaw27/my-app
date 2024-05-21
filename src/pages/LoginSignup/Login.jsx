f071ff4ec5c650a3f93b83bc2ad4e978389f7bf0
wiktorsaw27 <wiktorsawicki01@o2.pl>
2024-05-21 12:17:34 +0200

    logint update

--- src/pages/LoginSignup/Login.jsx
+++ src/pages/LoginSignup/Login.jsx
@@ -16,10 +16,45 @@ export const Login = () => {
 
   const loggin = async () => {
     console.log("Login function executed", formData);
+    let responseData;
+    await fetch('http://localhost:4000/login',{
+      method: 'POST',
+      headers:{
+        Accept: 'application/from-data',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(formData),
+    }).then((response)=>response.json()).then((data)=>responseData=data)
+
+    if(responseData.success) {
+      localStorage.setItem('auth-token', responseData.token);
+      window.location.replace("/");
+    }
+    else{
+      alert(responseData.errors);
+    }
+    
   }
 
   const signup = async () => {
     console.log("Signup function executed", formData);
+    let responseData;
+    await fetch('http://localhost:4000/signup',{
+      method: 'POST',
+      headers:{
+        Accept: 'application/from-data',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(formData),
+    }).then((response)=>response.json()).then((data)=>responseData=data)
+
+    if(responseData.success) {
+      localStorage.setItem('auth-token', responseData.token);
+      window.location.replace("/");
+    }
+    else{
+      alert(responseData.errors);
+    }
   }
 
   return (
@@ -33,8 +68,8 @@ export const Login = () => {
             </div>
             <button onClick={()=>{action==='Login'?loggin():signup()}}>Kontynuuj</button>
             {action==='Sign up'?
-            <p className="login-have">Masz już konto?<span onClick={()=>{setAction('Login')}}>Zaloguj się tutaj.</span> <hr/></p>:
-            <p className="login-have">Nie masz konta?<span onClick={()=>{setAction('Sign up')}}>Zarejestruj się tutaj.</span> <hr/></p>}
+            <p className="login-have">Masz już konto?<span onClick={()=>{setAction('Login')}}> Zaloguj się tutaj.</span> <hr/></p>:
+            <p className="login-have">Nie masz konta?<span onClick={()=>{setAction('Sign up')}}> Zarejestruj się tutaj.</span> <hr/></p>}
             <div className="login-agree">
                 <input type="checkbox" name='' id=''/>
                 <p>Wyrażam zgodę na przetwarzanie moich danych.</p>
