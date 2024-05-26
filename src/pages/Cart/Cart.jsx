9a8b2067f65d71d93cdcd05067672c458ea4cb0d
Szymon Kubica <szymon.kubica@srccode.pl>
2024-05-26 02:01:28 +0200

    kupowanie wersja 1

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -1,37 +1,42 @@
-import React, { useContext } from 'react'
-import './Cart.css'
-import { PRODUCTS } from '../../Products'
-import {ShopContext} from '../../Context/ShopContext'
-import { CartItem } from './CartItem'
-import {useNavigate} from 'react-router-dom'
+import React, { useContext } from 'react';
+import './Cart.css';
+import { PRODUCTS } from '../../Products';
+import { ShopContext } from '../../Context/ShopContext';
+import { CartItem } from './CartItem';
+import { useNavigate } from 'react-router-dom';
 
 export const Cart = () => {
-
     const { cartItems, getTotalCartAmount } = useContext(ShopContext);
     const totalAmount = getTotalCartAmount();
-    const navigate = useNavigate()
+    const navigate = useNavigate();
 
-  return (
-    <div className='cart'>
-        <div>
-            <h1>Twój Koszyka</h1>
+    const makePayment = () => {
+        // Replace with your actual Stripe payment link
+        const paymentLink = 'https://buy.stripe.com/test_cN2dRb1XrcNfcHmcMM';
+        window.location.href = paymentLink;
+    };
+
+    return (
+        <div className='cart'>
+            <div>
+                <h1>Twój Koszyk</h1>
+            </div>
+            <div className="cart-items">
+                {PRODUCTS.map((product) => {
+                    if (cartItems[product.id] !== 0) {
+                        return <CartItem key={product.id} data={product} />;
+                    }
+                })}
+            </div>
+            {totalAmount > 0 ? (
+                <div className="checkOut">
+                    <p>Total: {totalAmount} zł</p>
+                    <button onClick={() => navigate('/')}>Kontynuuj zakupy</button>
+                    <button onClick={makePayment}>Zapłać</button>
+                </div>
+            ) : (
+                <h1>Twój koszyk jest pusty</h1>
+            )}
         </div>
-        <div className="cart-items">
-            {PRODUCTS.map((product) => {
-                if (cartItems[product.id] !== 0){
-                    return <CartItem data={product} />
-                }
-            })}
-        </div>
-        {totalAmount > 0 ? (
-        <div className="checkOut">
-            <p>Total: {totalAmount} zł</p>
-            <button onClick={() => navigate('/')}>Kontynuuj zakupy</button>
-            <button>Zapłać</button>
-        </div>
-        ) : (
-            <h1>Twój koszyk jest pusty</h1>
-        )}
-    </div>
-  )
-}
+    );
+};
